596c857d0f3c3261e32ae99883a5a34c8f864c8f
Eng.Abdelrhman <79215297+Eldod0101@users.noreply.github.com>
2025-06-23 17:26:50 +0300

    refactor registration to store office data in user

--- lib/services/strapi_service.dart
+++ lib/services/strapi_service.dart
@@ -4,11 +4,18 @@ import 'package:http/http.dart' as http;
 class StrapiService {
   static const String baseUrl = 'http://192.168.1.12:1337';
 
+  /// API token with full privileges used for pre-account actions.
+  static const String adminApiToken =
+      '93474d3881c0274f78c97c281d3a178cfbb139fe90794f42f4ae45bf7aae5f6f0277050f1e27dcc841a35fe14fb14a9fc6ce35a4528fa0738767a6f5233cdaecf2c23b088cdb891316218a6af07a8cafb2b57f4cbdcc9e18bec5b959537b40e91541df94696c7183ebf30981ae209a78b27c2f55d283064cca8097ab774b2c2b';
+
   /// Uploads a file to Strapi and returns the media ID when successful.
-  Future<int?> uploadMedia(String filePath, String token) async {
+  ///
+  /// If no [token] is provided, the [adminApiToken] is used.
+  Future<int?> uploadMedia(String filePath, {String? token}) async {
+    final authToken = token ?? adminApiToken;
     final uri = Uri.parse('$baseUrl/api/upload');
     final request = http.MultipartRequest('POST', uri);
-    request.headers['Authorization'] = 'Bearer $token';
+    request.headers['Authorization'] = 'Bearer $authToken';
     request.files.add(await http.MultipartFile.fromPath('files', filePath));
     final response = await request.send();
     final responseBody = await response.stream.bytesToString();
